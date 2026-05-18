599992d13bca78d37a0a4adc643399b26f508f5f
Lynx Pardelle <lnxdrk@gmail.com>
2026-05-18 15:41:34 -0600

    Provide modal services for uploaders

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -19,6 +25,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
+    importProvidersFrom(ModalModule.forRoot()),
     ArticleService,
     BefService,
     MainService,
